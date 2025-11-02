8f329f62db7eb30eb5e168b8dacd1d0d84aeaa75
Albert Cervera i Areny <albert@nan-tic.com>
2025-11-02 11:57:31 +0100

    Fix message used for UserWarning.

--- invoice.py
+++ invoice.py
@@ -26,7 +26,7 @@ class Invoice(metaclass=PoolMeta):
             if Warning.check(key):
                 raise UserWarning(key,
                     gettext('account_invoice_vat_required.'
-                        'msg_party_tax_identifier', invoices=names))
+                        'msg_missing_party_tax_identifier', invoices=names))
 
         to_raise = [x for x in invoices if not x.tax_identifier]
         if to_raise:
