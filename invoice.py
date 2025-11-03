a6719452e951737211377e031bcb03c0abff1a5d
nan-tic-dev <79672209+nan-tic-dev@users.noreply.github.com>
2025-11-03 23:49:07 +0100

    Ensure invoice has a company tax identifier, do the checks after the call to super and add a test

--- invoice.py
+++ invoice.py
@@ -1,7 +1,7 @@
 from trytond.pool import PoolMeta, Pool
-from trytond.model import ModelView, fields
+from trytond.model import ModelView
 from trytond.i18n import gettext
-from trytond.exceptions import UserWarning
+from trytond.exceptions import UserError, UserWarning
 
 
 class Invoice(metaclass=PoolMeta):
@@ -12,18 +12,27 @@ class Invoice(metaclass=PoolMeta):
     def post(cls, invoices):
         pool = Pool()
         Warning = pool.get('res.user.warning')
+
+        super().post(invoices)
+
         to_warn = []
         for invoice in invoices:
             if not invoice.party.vat_required:
                 continue
             if not invoice.party_tax_identifier:
                 to_warn.append(invoice)
+
         if to_warn:
-            key = Warning.format('not_tax_identifier', to_warn)
-            names = ', '.join(
-                [invoice.rec_name for invoice in to_warn])
+            key = Warning.format('missing_tax_identifier', to_warn)
+            names = ', '.join([x.rec_name for x in to_warn])
             if Warning.check(key):
                 raise UserWarning(key,
-                    gettext('account_invoice_vat_required.msg_not_tax_identifier',
-                            invoices=names))
-        super().post(invoices)
+                    gettext('account_invoice_vat_required.'
+                        'msg_missing_party_tax_identifier', invoices=names))
+
+        to_raise = [x for x in invoices if not x.tax_identifier]
+        if to_raise:
+            names = ', '.join([x.rec_name for x in to_raise])
+            raise UserError(gettext('account_invoice_vat_required.'
+                    'msg_missing_company_tax_identifier', invoices=names))
+
